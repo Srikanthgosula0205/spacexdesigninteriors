4d35ab19671b4f1c53ef82b9a1e9346b7331c32a
Srikanthgosula0205 <info@spacexdesigninteriors.com>
2025-11-02 14:31:17 +0530

    Update manifest.js
    images added

--- manifest.js
+++ manifest.js
@@ -1,22 +1,64 @@
-﻿window.__MEDIA = {
+window.__MEDIA = {
   "Woodworks": [
-    { type: "image", src: "assets/woodworks/wood1.jpg" },
-    { type: "image", src: "assets/woodworks/wood2.jpg" },
-    { type: "image", src: "assets/woodworks/wood3.jpg" },
-    { type: "image", src: "assets/woodworks/wood4.jpg" },
-    { type: "image", src: "assets/woodworks/wood5.jpg" },
-    { type: "image", src: "assets/woodworks/wood6.jpg" }
+    {
+      "type": "image",
+      "src": "assets/woodworks/Wood1.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/woodworks/Wood2.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/woodworks/wood3.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/woodworks/wood4.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/woodworks/wood5.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/woodworks/wood6.jpg"
+    }
   ],
   "False Ceiling Designs": [
-    { type: "image", src: "assets/falseceiling/ceil1.jpg" },
-    { type: "image", src: "assets/falseceiling/ceil2.jpg" },
-    { type: "image", src: "assets/falseceiling/ceil3.jpg" },
-    { type: "image", src: "assets/falseceiling/ceil4.jpg" },
-    { type: "image", src: "assets/falseceiling/ceil5.jpg" }
+    {
+      "type": "image",
+      "src": "assets/falseceiling/ceil1.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/ceil2.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/ceil3.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/ceil4.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/ceil5.jpg"
+    }
   ],
   "Wallpapers": [
-    { type: "image", src: "assets/wallpapers/wall1.jpg" },
-    { type: "image", src: "assets/wallpapers/wall2.jpg" },
-    { type: "image", src: "assets/wallpapers/wall3.jpg" }
+    {
+      "type": "image",
+      "src": "assets/wallpapers/wall1.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/wallpapers/wall2.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/wallpapers/wall3.jpg"
+    }
   ]
 };
