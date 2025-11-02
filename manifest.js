107a75d613b76d4c75acf61cf2293a4581f23672
Srikanthgosula0205 <info@spacexdesigninteriors.com>
2025-11-02 14:48:11 +0530

    Update manifest.js
    Images are added

--- manifest.js
+++ manifest.js
@@ -65,21 +65,78 @@ window.__MEDIA = {
     {
       "type": "image",
       "src": "assets/falseceiling/ceil5.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/fal1.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/fal2.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/fal3.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/fal4.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/fal5.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/fal6.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/fal7.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/fal8.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/fal9.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/fal0.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/fal1.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/falseceiling/fal12.jpg"
     }
   ],
   "Wallpapers": [
     {
       "type": "image",
-      "src": "assets/wallpapers/wall1.jpg"
+      "src": "assets/wallpapers/wal1.jpg"
     },
     {
       "type": "image",
-      "src": "assets/wallpapers/wall2.jpg"
+      "src": "assets/wallpapers/wal2.jpg"
     },
     {
       "type": "image",
-      "src": "assets/wallpapers/wall3.jpg"
+      "src": "assets/wallpapers/wal3.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/wallpapers/wal4.jpg"
+    },
+    {
+      "type": "image",
+      "src": "assets/wallpapers/wal5.jpg"
     }
   ]
 };
+
 
